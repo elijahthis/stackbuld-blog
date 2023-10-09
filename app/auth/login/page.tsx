df5c8ccc12fbc50205315a8e8deb61078641e898
elijahthis <emperorelijah007@gmail.com>
2023-10-09 19:29:45 +0100

    fix: escaped &apos;

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -68,7 +68,7 @@ const Login = () => {
 					Login
 				</Button>
 				<p className="text-center text-sm">
-					Don't have an account?{" "}
+					Don&apos;t have an account?{" "}
 					<Link href="/auth/register" className="text-[#a4634d] ml-1">
 						Register
 					</Link>
